f440e797e6e16e8dd51cf809bd3c8b69e310a25a
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-06 16:28:49 -0500

    exercises

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+  $(".red-box").fadeTo(3000, 0.2);
+  $(".green-box").fadeTo(3000, 0.5);
+  $(".blue-box").fadeTo(3000, 0.8);
+
+  $(".blue-box").fadeToggle();
+  $(".blue-box").fadeToggle();
+});
