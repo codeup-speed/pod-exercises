5a04790a5ad294b862481d064c45692a9a7c8c8f
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-13 13:33:06 -0500

    bonus exercise

--- js/script.js
+++ js/script.js
@@ -5,4 +5,12 @@ $(function() {
 
   $(".blue-box").fadeToggle();
   $(".blue-box").fadeToggle();
+  $(".blue-box2").toggle();
+  $(".blue-box").hide(1000);
+  $(".blue-box").show(1000);
+  $(".blue-box2").slideUp(2000);
+  $(".blue-box2").slideDown(2000);
+  $("p").hide();
+  $("p").slideDown(3000);
+    $(".blue-box").slideToggle();
 });
